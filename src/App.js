7e4e4847812938305626a1e63e52b12630ca9abc
dongjun <sdj799@naver.com>
2023-06-12 15:49:04 +0900

    # 2. 이벤트와 스타일

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Hello from './Hello';
 import SayHello from './SayHello';
+import ChangeEvent from './component/chap01_event/ChangeEvent';
+import ClickEvent from './component/chap01_event/ClickEvent';
+import SubmitEvent from './component/chap01_event/SubmitEvent';
 
 //리엑트에서는 하나의 컴포넌트가 여러개의 엘리먼트를 반환할 수 있습니다.
 //JSX를 작성할 때 return 문 안에 반드시 하나의 최상위 태그가 있어야 합니다.
@@ -20,6 +23,9 @@ function App() {
   return (
     <> 
       { looping() }
+      <ChangeEvent/>
+      <ClickEvent/>
+      <SubmitEvent/>
       <br/>
       <Hello/>
     </>
